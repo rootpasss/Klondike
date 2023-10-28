8c90a5b06ebef8b69443baeaa4321106d77711e8
rootpasss <jtrejosb@live.com>
2023-10-28 10:20:31 -0500

    keystroke block code moved to controller class

--- src/main/io/bitlab/api/view/GameView.java
+++ src/main/io/bitlab/api/view/GameView.java
@@ -66,17 +66,6 @@ public class GameView extends JFrame {
         g2.drawImage(back,0,0,null);
       }
     };
-    /*contentPane.getInputMap(contentPane.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT)
-               .put(javax.swing.KeyStroke.getKeyStroke("S"),"stats");
-    contentPane.getActionMap().put("stats",new javax.swing.AbstractAction() {
-      @Override
-      public void actionPerformed(ActionEvent evt) {
-        //RecordStore rs=RecordStore.openRecordStore();
-        String stats="<html><h2><b>Games Played: "+rs.getRecord()[1]+"</b></h2><h2><b>Games Won: "+
-                     RecordStore.getRecord()[2]+"</b></h2></p></html>";
-        JOptionPane.showMessageDialog(contentPane,stats,"Solitaire Stats",JOptionPane.PLAIN_MESSAGE);
-      }
-    });*/
     add(contentPane);
     loadCards();
     createArea();
