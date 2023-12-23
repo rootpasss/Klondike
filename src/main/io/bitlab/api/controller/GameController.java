a8e22033310c6f595bdf82ab2c871c3223c026aa
rootpasss <jtrejosb@live.com>
2023-12-23 12:12:58 -0500

    subtract 2 points to score each 10 seconds

--- src/main/io/bitlab/api/controller/GameController.java
+++ src/main/io/bitlab/api/controller/GameController.java
@@ -200,7 +200,10 @@ public class GameController {
       int c=0;
       @Override
       public void run() {
-        gv.updateTime(c++);
+        c++;
+        gv.updateTime(c);
+        if(c%10==0)
+          gv.updateScore(ge.subtractScore());
       }
     },0,1000);
     return t;
